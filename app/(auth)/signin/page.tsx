c2c989157f8e6f6acabf5004e2f0b7dc26bd2f91
x <abhishek@xs-Mac-mini.local>
2025-04-07 14:57:58 +0530

    ASD

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,5 +1,5 @@
 export const metadata = {
-  title: "Sign In - Open PRO",
+  title: "Sign In - Glinthawk technology",
   description: "Page description",
 };
 
--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Home - Open PRO",
-  description: "Page description",
+  title: "Home - Glinthawk technology",
+  description: "Best Product Development Company at very affordable price",
 };
 
 import PageIllustration from "@/components/page-illustration";
